d1010ae205e4f3423e6fbd15e38d9bba9fe62a93
sojoswapdev <117547650+sojoswapdev@users.noreply.github.com>
2022-11-05 16:08:30 -0700

    Delete utils directory

--- utils/currencyId.ts
+++
@@ -1,7 +0,0 @@
-import { Currency, NativeCurrency, Token } from '@uniswap/sdk'
-
-export function currencyId(currency: Currency): string {
-  if (currency instanceof NativeCurrency) return 'ETH'
-  if (currency instanceof Token) return currency.address
-  throw new Error('invalid currency')
-}
